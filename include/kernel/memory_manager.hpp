08b2ed8470ea63b1cc4a3614573c005447772591
Tomáš Petříček <tomaspetricek@Tomas--MacBook-Air.local>
2024-10-05 20:31:01 +0200

    create constructor

--- include/kernel/memory_manager.hpp
+++ include/kernel/memory_manager.hpp
@@ -1,15 +1,37 @@
 #ifndef KERNEL_MEMORY_MANAGER_HPP
 #define KERNEL_MEMORY_MANAGER_HPP
 
-#include <cstdint>
 #include <cstddef>
+#include <cstdint>
+
+#include <common/stdlib.h>
 
 namespace tp
 {
     using memory_t = void *;
+    using address_t = uint32_t;
 
     class heap_allocator
     {
+        struct heap_segment
+        {
+            struct heap_segment *next{nullptr};
+            struct heap_segment *prev{nullptr};
+            bool is_allocated{false};
+            uint32_t segment_size{0}; // includes this header
+        };
+
+        heap_segment *list_head_{nullptr};
+
+    public:
+        explicit heap_allocator(address_t heap_start,
+                                std::size_t heap_size) noexcept
+        {
+            list_head_ = (heap_segment *)heap_start;
+            bzero(list_head_, sizeof(heap_segment));
+            list_head_->segment_size = heap_size;
+        }
+
         memory_t allocate(std::size_t amount) noexcept {}
 
         void deallocate(memory_t memory) noexcept {}
