d0eba707ab27ef3405ec1a2b69cfc8ec21c0eb43
Tomáš Petříček <tomaspetricek@Tomas--MacBook-Air.local>
2024-10-05 14:06:45 +0200

    create memory management abstraction

---
+++ include/kernel/memory_manager.hpp
@@ -0,0 +1,37 @@
+#ifndef KERNEL_MEMORY_MANAGER_HPP
+#define KERNEL_MEMORY_MANAGER_HPP
+
+#include <cstdint>
+#include <cstddef>
+
+namespace tp
+{
+    using memory_t = void *;
+
+    class heap_allocator
+    {
+        memory_t allocate(std::size_t amount) noexcept {}
+
+        void deallocate(memory_t memory) noexcept {}
+    };
+
+    class page_allocator
+    {
+        memory_t allocate() noexcept {}
+
+        void deallocate(memory_t memory) noexcept {}
+    };
+
+    class memory_manager
+    {
+        memory_t allocate_page() noexcept {}
+
+        void deallocate_page(memory_t memory) noexcept {}
+
+        void *allocate(std::size_t amount) noexcept {}
+
+        void deallocate(memory_t memory) noexcept {}
+    };
+} // namespace tp
+
+#endif // KERNEL_MEMORY_MANAGER_HPP
